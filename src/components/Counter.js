a5921416c300a49a7e21771ad2d7b726437bf9d9
ckirk2004 <ckirk2004@gmail.com>
2020-12-16 20:00:55 -0600

    completed moods.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -66,7 +66,7 @@ const [count, setCount] = useState(0);
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: count % 2 === 0 ? 'royalblue' : 'crimson' /* STEP 2 */
+    color: count % 2 === 0 ? 'green' : 'orange' /* STEP 2 */
   };
 
   return (
